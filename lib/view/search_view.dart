1c829ab86ceaaccd2ec73427054460065de3806c
Mostafaali <mostafa@gmail.com>
2024-07-25 15:13:08 +0300

    last

--- lib/view/search_view.dart
+++ lib/view/search_view.dart
@@ -13,46 +13,34 @@ class SearchView extends StatefulWidget {
 }
 
 class _SearchViewState extends State<SearchView> {
-  var formkey = GlobalKey<FormState>();
-  TextEditingController controller = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      key: formkey,
       appBar: AppBar(
         title: const Text(
           "Search City",
           style: TextStyle(fontSize: 25),
         ),
       ),
-      body: Form(
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 15),
-            child: TextFormField(
-              validator: (value) {
-                if (value!.isEmpty) {
-                  return 'Name is empty';
-                }
-                return null;
-              },
-              onFieldSubmitted: (value) async {
-                if (value != null) {
-                  Navigator.pop(context);
-                  var getWeather = BlocProvider.of<GetWeatherCubit>(context);
-                  getWeather.getWeather(cityName: value);
-                } else {}
-              },
-              controller: controller,
-              decoration: InputDecoration(
-                contentPadding:
-                    const EdgeInsets.symmetric(vertical: 25, horizontal: 16),
-                suffixIcon: const Icon(Icons.search),
-                labelText: "Search",
-                hintText: "Enter your city",
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(10),
-                ),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: TextFormField(
+            onFieldSubmitted: (value) async {
+              if (value != null) {
+                Navigator.pop(context);
+                var getWeather = BlocProvider.of<GetWeatherCubit>(context);
+                getWeather.getWeather(cityName: value);
+              } else {}
+            },
+            decoration: InputDecoration(
+              contentPadding:
+                  const EdgeInsets.symmetric(vertical: 25, horizontal: 16),
+              suffixIcon: const Icon(Icons.search),
+              labelText: "Search",
+              hintText: "Enter your city",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10),
               ),
             ),
           ),
